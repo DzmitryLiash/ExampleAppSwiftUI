6c341d18038ee3c9f3b9e87a8138c07994e14ff0
Dzmitry Liashchou <liashchou.d@gmail.com>
2022-01-05 14:11:59 +0100

    Initial Commit

---
+++ LandmarksC/LandmarksCApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksCApp.swift
+//  LandmarksC
+//
+//  Created by Дмитрий Лещёв on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
